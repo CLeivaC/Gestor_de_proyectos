16dd2c55cc921f875162f2f76df40a2a29b7b00d
ChristianLeiva <cristhian.leiva.pc@outlook.com>
2024-09-12 23:12:27 +0200

    Conexión a base de datos establecida

---
+++ Data/ConexionDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_proyectos.Data
+{
+    public class ConexionDB
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static ConexionDB Con = null;
+
+        private ConexionDB() 
+        {
+            this.Servidor = "DESKTOP-NRFRB6B\\SQLEXPRESS";
+            this.Base = "GestionProyectos";
+            this.Usuario = "leiva";
+            this.Clave = "1234";
+        }
+
+        public SqlConnection CrearConexionDB() 
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor +
+                                          "; Database= " + this.Base +
+                                          "; User id=" + this.Usuario +
+                                          "; Password =" + this.Clave;
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static ConexionDB crearInstanciaDB()
+        {
+            if (Con == null)
+            {
+                  Con = new ConexionDB();
+            }
+            return Con;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Gestion_de_proyectos.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_proyectos
+{
+      public class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlCon = new SqlConnection();
+            sqlCon = ConexionDB.crearInstanciaDB().CrearConexionDB();
+
+           
+
+            try
+            {
+                sqlCon.Open();
+                Console.WriteLine("Conexión exitosa");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Conexión fallida");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
